8e845213b58894421d2f954be9c430d1044410a4
Nino <ninotroia96@gmail.com>
2023-07-11 17:44:59 +0200

    header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,30 @@
 <div class="container-header">
-    <h1>COMICS</h1>
+    <h1>DC Power Visa           Additional DC sites</h1>
 </div>
+<div class="container-nav">
+    <div class="logo">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="Logo">
+    </div>
+
+</div>
+
 
 <style>
     .container-header {
-        height : 150px ;
+        height : 30px ;
+        
         background-color : blue;
         color: white;
-       
+        font-size: 7px;
         display: flex;
         align-items: center;
-        justify-content: center
-    }
+        justify-content: end;
+        padding-right: 50px
+        
+    };
+
+   
+
+  
+    
 </style>
